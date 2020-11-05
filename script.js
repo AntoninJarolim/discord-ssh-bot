f267dafd046f15a56b5f8378cba2b45278e949d6
AntoninJarolim <jarolim.antonin@seznam.cz>
2020-11-05 14:01:54 +0100

    robmu email spravny

--- script.js
+++ script.js
@@ -82,7 +82,7 @@ client.on("message", msg => {
 		let url = msg.attachments.first().url;
 		download(url, name);
 		
-		exec("sleep 3; echo \"\" | mutt -s \"skeeero\" -a ./"+ name +" -- jarolim.antonin@seznam.cz");
+		exec("sleep 3; echo \"\" | mutt -s \"skeeero\" -a ./"+ name +" -- vm56fy38@hpeprint.com");
 
 		msg.channel.send("Email odeslan :)");
 		return 0;
